5b468ee52f47f00d35745cacf09e3c3f737407a8
david.van.steertegem <david.van.steertegem@ehb.be>
2023-04-24 15:37:25 +0200

    created Snack class

--- src/be/ehb/Main.java
+++ src/be/ehb/Main.java
@@ -3,6 +3,7 @@ package be.ehb;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        System.out.println("Hello world");
     }
 }
